9abe9e9c557f8bb254588fb3714b7144944c9499
hetpatel2508 <125752460+hetpatel2508@users.noreply.github.com>
2024-02-08 16:24:53 +0530

    .

---
+++ Collage Practicals/pr12_1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * Test
+ */
+class school {
+    int id;
+    String name;
+    static String Course_name;
+
+    school(int i, String n) {
+        id = i;
+        name = n;
+    }
+
+    static {
+        Course_name = "Computer Engineering";
+    }
+
+};
+
+public class pr12_1 {
+
+    static String hello = "Hello World";
+
+    public static void main(String[] args) {
+           System.out.println(hello); 
+
+           school s1 = new school(1,"Het");
+           school s2 = new school(1,"Sahil");
+
+           System.out.println("Id = "+s1.id+"  Name = "+s1.name+" Course Name = "+school.Course_name);
+           System.out.println("Id = "+s2.id+"  Name = "+s2.name+" Course Name = "+school.Course_name);
+    }
+}
---
+++ Collage Practicals/pr15_1.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+/**
+ * Test
+ */
+class student {
+    int id;
+    String name;
+
+    student(int i, String n)
+    {
+        id=i;
+        name=n;
+    }
+};
+
+class teacher extends student {
+    int attendance;
+
+    teacher(int a, int i, String n) {
+        super(i,n);
+        attendance = a;
+    }
+};
+
+class school extends teacher {
+    String fees_Details;
+
+    school(String f, int a, int i, String n) {
+        super(a, i, n);
+        fees_Details = f;
+    }
+
+    void output()
+    {
+        System.out.println("Id = "+id);
+        System.out.println("Name = "+name);
+        System.out.println("Attendance = "+attendance);
+        System.out.println("Fees_Details = "+fees_Details);
+        System.out.println("");
+    }
+};
+
+public class pr15_1 {
+    public static void main(String[] args) {
+        school s1 = new school("Paid", 50, 30273, "Het");
+        s1.output();
+    }
+}

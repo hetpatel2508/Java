28dfdb6b326709ff38e6e97d475f2c1644b5f3d5
hetpatel2508 <125752460+hetpatel2508@users.noreply.github.com>
2024-03-10 23:46:15 +0530

    .

---
+++ static_nested_classes.java
@@ -0,0 +1,34 @@
+class student {
+    int id;
+    String name;
+
+    static class school {
+        student s = new student();
+        String feesPaid;
+
+        
+        school(int i, String n, String f) {
+            s.id = i;
+            s.name = n;
+            feesPaid = f;
+        }
+
+        void output() {
+            System.out.println("Id = " + s.id);
+            System.out.println("Name = " + s.name);
+            System.out.println("Fees = " + feesPaid);
+            System.out.println("");
+        }
+    }
+}
+
+public class static_nested_classes {
+    public static void main(String[] args) {
+        
+        student.school inner = new student.school(30273, "HetPatel", "Paid");
+        inner.output();
+
+        student.school inner1 = new student.school(30211, "SahilPatel", "unPaid");
+        inner1.output();
+    }
+}
